a3c28f1fa5517e128dd127e164b6dbdc9d26a146
Federico Gonzalez <fedegl@hey.com>
2022-04-24 19:59:27 -0700

    Configure bali modal component

--- app/javascript/documentation/application.js
+++ app/javascript/documentation/application.js
@@ -3,13 +3,18 @@ import '@hotwired/turbo-rails'
 import { Application } from '@hotwired/stimulus'
 import { NotificationController, SlimSelectController } from 'frontend-helpers'
 
-import { DropdownController, TabsController } from 'bali-view-components'
+import {
+  DropdownController,
+  ModalController,
+  TabsController
+} from 'bali-view-components'
 
 import RichTextEditorController from './controllers/rich_text_editor_controller'
 
 const application = Application.start()
 
 application.register('dropdown', DropdownController)
+application.register('modal', ModalController)
 application.register('notification', NotificationController)
 application.register('rich-text-editor', RichTextEditorController)
 application.register('slim-select', SlimSelectController)
