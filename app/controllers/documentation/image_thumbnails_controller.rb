8c4ea9aa4832f19168e6a782a71f45686590e81f
Federico Gonzalez <fedegl@hey.com>
2022-05-13 17:12:05 -0700

    Cleanup

--- app/controllers/documentation/image_thumbnails_controller.rb
+++ app/controllers/documentation/image_thumbnails_controller.rb
@@ -1,15 +1,9 @@
 module Documentation
   class ImageThumbnailsController < ApplicationController
-    before_action :set_page
-
     def index
-      @images = @page.images
+      @page = Page.find(params[:page_id])
 
       render layout: false
-    end
-
-    def set_page
-      @page = Page.find(params[:page_id])
     end
   end
 end
